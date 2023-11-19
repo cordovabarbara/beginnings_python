2e2868d6818c21dcc7e813a496709e1596528b57
cordovabarbara <aliendocordova@gmail.com>
2023-11-19 16:09:13 -0500

    principios de python variables

---
+++ 00_hello.py
@@ -0,0 +1,14 @@
+# esto es un comentario Hola mundo
+"""
+este es un comentario en 
+varias lineas
+"""
+print("Hola Mundo")
+
+# consultar tipo de datos
+print(type('soy un dato')) #Tipo 'str'
+print(type(5)) #Tipo inter
+print(type(5.2)) #Tipo decimal
+print(type(True)) #Tipo boo
+print(type(1 +2j)) #Tipo complex
+
---
+++ 01_variable.py
@@ -0,0 +1,33 @@
+#Variables
+my_variable ='My String Variable'
+print(my_variable)
+
+my_int_variable = 5
+print(my_int_variable)
+
+is_married = True
+print(is_married)
+
+my_int_to_str_variable = str(my_int_variable)
+print(my_int_to_str_variable)
+print(type(my_int_to_str_variable))
+
+#Concatenacion  de variables en un print
+print(my_variable,(my_int_variable))
+print('Este es el valor de:', is_married)
+
+#Funcion del sistema
+print(len(my_variable))
+
+#Variables en una sola linea ¡no abusar!
+name, surname, alias, age = "Barbara", "Cordova", "Barby", 31
+print("Me llamo:",name, surname,".Mi edad es:",age,".Y mi alias es", alias)
+
+#Inputs
+"""
+first_name = input('What is your name: ')
+age = input('How old are you? ')
+
+print(first_name)
+print(age)
+"""
