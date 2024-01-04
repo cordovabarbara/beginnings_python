561a10673f29fa30b232a3790c950dc338ffefd1
cordovabarbara <aliendocordova@gmail.com>
2024-01-03 19:41:22 -0500

    operadores logicos

--- 02_operators.py
+++ 02_operators.py
@@ -8,3 +8,25 @@ print(10 % 2)
 print(10//3) #division con dos barras, aprox a num entero
 print(2**3) #exponente - dos elevado a tres
 
+print("Hola" + str(5))
+print("Hola "  * 5)
+
+#operadores comparativos#
+print(3 > 4)
+print(3 < 4)
+print(3 >= 4)
+print(4 <= 4)
+print(3 == 4)
+print(3 != 4)
+
+print("Hola" > "Python")
+print("Hola" < "Python")
+print("Hola" >= "Python") #ordernacion alfabetica por ASCII
+print("Hola"<= "Python")
+print("Hola" == "Hola")
+print("Hola" != "Python")
+
+#operadores Logicos#
+print(3 > 4 and "Hola" > "Python")
+print(3 < 4 or "Hola" < "Python")
+print(not(3 > 4)) #es para negar, contrario
