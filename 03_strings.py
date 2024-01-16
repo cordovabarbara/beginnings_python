96c005c0a1d82f98f15b22b6423f69b90a74b0e5
cordovabarbara <aliendocordova@gmail.com>
2024-01-15 19:20:53 -0500

    divison y reverso de string

--- 03_strings.py
+++ 03_strings.py
@@ -11,3 +11,21 @@ print(my_new_line_string)
 name, surname, age = "Barbara", "Cordova", 31
 print("Mi nombre es {} {} y mi edad es {}". format(name, surname, age))
 print("Mi nombre es %s %s y mi edad es %d" %(name, surname, age))
+
+#inferencia de datos
+print(f"Mi nombre es {name} {surname} y mi edad es {age}") #mas rapida y mas util
+
+# Desempaquetado de caracteres  
+lenguage = "Python"
+a,b, c, d, e, f = lenguage
+print(a)
+print(f)
+
+#Division
+
+lenguage_slice = lenguage[0:3]
+print(lenguage_slice)
+
+#reverse
+reverse = lenguage [::-1]
+print(reverse)
