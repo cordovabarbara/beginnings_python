d6e0c7096e26e42c011820de8053426050769401
cordovabarbara <aliendocordova@gmail.com>
2024-01-22 18:21:35 -0500

    funciones y listas

--- 03_strings.py
+++ 03_strings.py
@@ -13,10 +13,11 @@ print("Mi nombre es {} {} y mi edad es {}". format(name, surname, age))
 print("Mi nombre es %s %s y mi edad es %d" %(name, surname, age))
 
 #inferencia de datos
-print(f"Mi nombre es {name} {surname} y mi edad es {age}") #mas rapida y mas util
+print(f"Mi nombre es {name} {surname} y mi edad es {age}") #mas rapida y mas util - interpolacion
+print(f"Mi apellido es {surname} y mi nombre es {name}")
 
 # Desempaquetado de caracteres  
-lenguage = "Python"
+lenguage = "python"
 a,b, c, d, e, f = lenguage
 print(a)
 print(f)
@@ -29,3 +30,14 @@ print(lenguage_slice)
 #reverse
 reverse = lenguage [::-1]
 print(reverse)
+
+#funciones
+
+print(lenguage.capitalize())
+print(lenguage.upper())
+print(lenguage.count("p"))
+print(lenguage.isnumeric())
+print(lenguage.lower())
+print(lenguage.isupper())
+print(lenguage.startswith("tho"))
+print(lenguage.startswith("py"))
---
+++ 04_listas.py
@@ -0,0 +1,13 @@
+# Listas #
+
+#lista es una caja para agregar, agrupar datos#
+
+my_list = list()
+my_other_list = [35, 24, 31, 30, 18, 30]
+
+print(len(my_other_list))
+
+my_data = [35, 1.16, "Barbara", "Cordova"
+]
+
+print(type(my_data))
