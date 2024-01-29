885be0d58640005b30ca6cfddcc6f5e24e1e5519
cordovabarbara <aliendocordova@gmail.com>
2024-01-29 14:34:25 -0500

    sets metodos

---
+++ 06_sets.py
@@ -0,0 +1,52 @@
+## set ##
+
+my_set = set()
+my_other_set = {}
+
+print(type(my_set))
+
+my_other_set = {"Barbara", "Cordova", 31}
+
+print(type(my_other_set))
+
+print(len(my_other_set))
+
+my_other_set.add("Anais")
+
+print(my_other_set) ## un set no es una estructura ordenada, ni admite repetidos
+
+print("Moure" in my_other_set)
+print(31 in my_other_set)
+
+my_other_set.remove("Cordova")
+print(my_other_set)
+
+my_other_set.clear()
+print(my_other_set)
+
+my_set = {"Barbara", "Cordova", 31}
+
+my_other_set = {"javascript", "ingles", "react"}
+
+my_new_set = my_set.union(my_other_set)
+print(my_new_set)
+
+print(my_new_set.difference(my_set))
+
+it_companies = {'Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon'}
+
+print(len(it_companies))
+
+it_companies.add("Twitter")
+print(it_companies)
+
+new_companies = {"Linux", "Continuum", "Globant"}
+
+it_companies.update(new_companies)
+print(it_companies)
+
+it_companies.discard("Continuum")
+print(it_companies)
+
+it_companies.remove('Amazon')
+print(it_companies)
