be2cbde5b3190b68722ac634a4036634a1d72643
cordovabarbara <aliendocordova@gmail.com>
2024-02-26 18:34:59 -0500

    ejemplos de diccionario

--- 07_dictionaries.py
+++ 07_dictionaries.py
@@ -1,6 +1,19 @@
 ##Dictionaries##
+#Clave valor
 
 my_dict = dict()
 print(type(my_dict))
 
-my_new_dict = {"Nombre":"Anais", "Apellido":"Cordova", "Edad":31}
+my_new_dict = {"Nombre":"Anais", "Apellido":"Cordova", "Edad":31, 1:"Python"}
+print(my_new_dict)
+
+my_dict = {
+    "Nombre":"Anais",
+    "Apellido":"Cordova", 
+    "Edad":31, 
+    "Lenguajes":{"Python", "Javascript", "kotlin"},
+    1:1.69
+}
+print(my_dict)
+
+print(len(my_dict))
