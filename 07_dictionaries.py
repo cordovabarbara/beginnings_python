fd87d2a33de34873f7eaecdab606a4a7c40092b4
cordovabarbara <aliendocordova@gmail.com>
2024-02-26 19:48:48 -0500

    mas ejemplos

--- 07_dictionaries.py
+++ 07_dictionaries.py
@@ -16,4 +16,29 @@ my_dict = {
 }
 print(my_dict)
 
-print(len(my_dict))
+print(my_dict["Nombre"])
+
+my_dict["Nombre"] = "Leonor"
+print(my_dict["Nombre"])  
+##Se puede acceder a una clave y cambiar el valor##
+print(my_dict[1])
+
+my_dict["Direccion"] = "Bogota"
+print(my_dict)
+
+del my_dict ["Direccion"]
+print(my_dict)
+
+print("Cordova" in my_dict) ##asi se esta buscando por clave no por valor##
+
+print("Apellido" in my_dict)
+
+print(my_dict.items ()) #listado de casa uno de los items
+print(my_dict.keys ()) #listado de keys
+print(my_dict.values ()) ## retorna todos los valores
+
+a_new_dict = my_dict.fromkeys(my_dict)
+print(a_new_dict)
+
+print(list(a_new_dict))
+
