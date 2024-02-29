56b9f17b92b9f67c5abab1c554a23eca5ecb622a
cordovabarbara <aliendocordova@gmail.com>
2024-02-29 18:55:30 -0500

    condicionales: if

---
+++ 08_conditionals.py
@@ -0,0 +1,14 @@
+## Condicionales
+##si se cumple lo que esta en la condicion, se ejecuta la accion que esta adentro
+
+my_condition = False
+
+if my_condition:
+    print("Se ejecuta la condicion del if")
+
+my_condition = 5 * 2
+
+if my_condition >= 10:
+    print("Se ejecuta el segundo if")
+
+print("La ejecucion continua")
