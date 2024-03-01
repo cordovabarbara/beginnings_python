61e04fd1214a14fd6228cb77f62e2db3434dd6d6
cordovabarbara <aliendocordova@gmail.com>
2024-02-29 19:21:36 -0500

    else y elif

--- 08_conditionals.py
+++ 08_conditionals.py
@@ -6,9 +6,22 @@ my_condition = False
 if my_condition:
     print("Se ejecuta la condicion del if")
 
-my_condition = 5 * 2
+my_condition = 5 * 3
 
 if my_condition >= 10:
     print("Se ejecuta el segundo if")
 
+if my_condition > 10 and my_condition < 20:
+    print("Es Mayor que 10")
+else:
+    print("Es menor o igual que 10 o mayor o igual que 20")
+
 print("La ejecucion continua")
+
+##elif necesita una condicion es para hacer algo en concreto
+
+my_string = "Mi texto corto"
+
+if my_string:
+    print("Mi cadena de texto no esta vacia")
+
