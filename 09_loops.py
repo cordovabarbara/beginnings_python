41f4e9e1049aec55ae76df033a9de5be0aa50aa4
cordovabarbara <aliendocordova@gmail.com>
2024-04-21 13:49:55 -0500

    bucle while

--- 09_loops.py
+++ 09_loops.py
@@ -3,6 +3,18 @@
 # While
 
 my_condition = 0
+
 while my_condition < 10:
     print(my_condition)
     my_condition += 2
+else:
+    print("Mi condicion es mayor o igual a 10")
+
+print("la ejecucion continua")
+
+while my_condition < 20:
+    my_condition += 1
+    if my_condition == 15:
+        print("Mi condicion es 15, se detiene")
+        break
+    
