fa79bb92ece8f4b651935f1d360f214d0e7ac9d5
cordovabarbara <aliendocordova@gmail.com>
2024-06-05 11:35:14 -0500

    clases

--- 11_classes.py
+++ 11_classes.py
@@ -1,4 +1,16 @@
 # Classes #
+#Creamos una clase para crear un objeto. Una clase es como un constructor de objetos o un "modelo" para crear objetos. Creamos una instancia de una clase para crear un objeto. La clase define los atributos y el comportamiento del objeto, mientras que el objeto, por otro lado, representa la clase.#
 
 class Person:
-    pass
+    def __init__(self, name, surname, alias):
+        #self.name = name
+        #self.surname = surname#
+        self.full_name = f"{name} {surname} ({alias}) "
+
+    def walk (self):
+        print(f"{self.full_name} esta caminado")
+
+
+my_person = Person( "Barbara", "Cordova", ("Barby"))
+print(my_person.full_name)
+my_person.walk()
