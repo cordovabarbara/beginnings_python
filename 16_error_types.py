23fc9a0ffa67225ced3d8f60a415907cbb9f30c6
cordovabarbara <aliendocordova@gmail.com>
2024-07-25 17:15:24 -0500

    NameError

--- 16_error_types.py
+++ 16_error_types.py
@@ -7,4 +7,7 @@ print (" Hola Comunidad")
 '''SyntaxError: Missing parentheses in call to 'print'. Did you mean print(...)?'''
 
 #NameError
-print(name)
+#NameError: name 'variable' is not defined. Did you mean: 'callable'?
+#print(variable)
+variable = 5
+print(variable)
