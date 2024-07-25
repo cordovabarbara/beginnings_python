17f87804accf51ed06c83fb72411fffd7c6da2df
cordovabarbara <aliendocordova@gmail.com>
2024-07-25 13:58:38 -0500

    Error Types

---
+++ 16_error_types.py
@@ -0,0 +1,10 @@
+# Error Types ##
+
+# SyntaxError 
+
+#print " Hola Comunidad"
+print (" Hola Comunidad")
+'''SyntaxError: Missing parentheses in call to 'print'. Did you mean print(...)?'''
+
+#NameError
+print(name)
