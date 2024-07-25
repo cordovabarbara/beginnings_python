5ffb9a7438dc98ee10a461dca3b2f20557614276
cordovabarbara <aliendocordova@gmail.com>
2024-07-25 17:32:31 -0500

    indexError y Module not found

--- 16_error_types.py
+++ 16_error_types.py
@@ -1,4 +1,4 @@
-# Error Types ##
+# Error Types #
 
 # SyntaxError 
 
@@ -11,3 +11,14 @@ print (" Hola Comunidad")
 #print(variable)
 variable = 5
 print(variable)
+
+# IndexError
+#IndexError: list index out of range 
+#print(my_list[5])
+my_list =["Python", "Javascript", "C++", "Dart"]
+print(my_list[3])
+
+# ModuleNotFoundError
+"""import maths
+ModuleNotFoundError: No module named 'maths'"""
+import math
