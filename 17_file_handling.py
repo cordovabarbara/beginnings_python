f00fcab223f8c529fac4dad79bb34674e86c793a
cordovabarbara <aliendocordova@gmail.com>
2024-07-26 14:50:26 -0500

    de json a diccionario

--- 17_file_handling.py
+++ 17_file_handling.py
@@ -27,3 +27,13 @@ my_json = {
 
 json.dump(my_json, json_file, indent = 2)
 
+json_file.close()
+
+with open("./my_file.json") as my_other_file:
+    for line in my_other_file.readlines():
+        print(line)
+
+json_dict = json.load(open("./my_file.json"))
+print(json_dict)
+
+print(type(json_dict))
