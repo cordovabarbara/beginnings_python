2504b2396a4ea457388833da15d40c8dc69384e5
cordovabarbara <aliendocordova@gmail.com>
2024-07-26 14:39:35 -0500

    manejo de json

--- 17_file_handling.py
+++ 17_file_handling.py
@@ -10,3 +10,20 @@ for line in txt_file.readlines():
     print(line)
 print(txt_file.readline())
 txt_file.close()
+
+# json file
+
+import json
+
+json_file = open("./my_file.json", "w+")
+
+my_json = {
+    "Nombre":"Anais",
+    "Apellido":"Cordova", 
+    "Edad":31, 
+    "Lenguajes":"Python",
+    "Profesion":"informatico"
+}
+
+json.dump(my_json, json_file, indent = 2)
+
