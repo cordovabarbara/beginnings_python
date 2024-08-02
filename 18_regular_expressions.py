f5c10847038e3c845341d74a2eafce20fa1a2a66
cordovabarbara <aliendocordova@gmail.com>
2024-08-02 13:25:33 -0500

    expresiones regulares, match

---
+++ 18_regular_expressions.py
@@ -0,0 +1,10 @@
+# Regular Expressions #
+
+import re
+
+my_string = "Esta es la leccion de expresiones regulares"
+
+my_other_string = "Esta no es la leccion de expresiones regulares"
+
+
+print(re.match("Esta es la leccion", my_string))
